1d24b97c2365b694b9beff37bc24413ddedc16d1
Konstantinos Kollias <kostas.kollias.gr@gmail.com>
2020-07-21 00:42:56 +0300

    attempting thread based mdc

--- src/test/java/examples/Amazing.java
+++ src/test/java/examples/Amazing.java
@@ -2,11 +2,13 @@ package examples;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class Amazing {
     private static final Logger logger = LoggerFactory.getLogger(Amazing.class);
 
     public static void doWork() {
+        MDC.put("logFileName", Thread.currentThread().getName());
         logger.info("Doing lots of work");
         System.out.println("Doing lots of work in system out");
 
